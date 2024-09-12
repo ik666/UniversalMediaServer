aec1e05f0a7c3fe58d40257da879330cfcc69785
ik666 <satfreak666@gmx.de>
2024-09-12 09:44:35 +0200

    checkstyle

--- src/main/java/net/pms/store/StoreResource.java
+++ src/main/java/net/pms/store/StoreResource.java
@@ -955,15 +955,17 @@ public abstract class StoreResource implements Cloneable, Runnable {
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
+		if (this == obj) {
 			return true;
-		if (obj == null)
+		}
+		if (obj == null) {
 			return false;
-		if (getClass() != obj.getClass())
+		}
+		if (getClass() != obj.getClass()) {
 			return false;
+		}
 		StoreResource other = (StoreResource) obj;
 		return Objects.equals(id, other.id);
 	}
 
-	
 }
