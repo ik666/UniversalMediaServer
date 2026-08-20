7efe75301c3f48562dd1c7c7ac785e13f2813238
ik666 <satfreak666@gmx.de>
2026-08-20 11:52:48 +0200

    DLNA thumb cache

--- src/main/java/net/pms/network/mediaserver/servlets/MediaServerServlet.java
+++ src/main/java/net/pms/network/mediaserver/servlets/MediaServerServlet.java
@@ -64,6 +64,7 @@ import net.pms.store.IcyMetadataSource;
 import net.pms.store.MediaStoreIds;
 import net.pms.store.StoreItem;
 import net.pms.store.StoreResource;
+import net.pms.store.ThumbnailStore;
 import net.pms.util.ByteRange;
 import net.pms.util.FullyPlayed;
 import net.pms.util.Range;
@@ -813,7 +814,8 @@ public class MediaServerServlet extends MediaServerHttpServlet {
 		filterChain = resource.addFlagFilters(filterChain);
 
 		if (thumbInputStream != null) {
-			inputStream = thumbInputStream.transcode(
+			inputStream = ThumbnailStore.getTranscodedThumbnailInputStream(
+					thumbInputStream,
 					imageProfile,
 					renderer.isThumbnailPadding(),
 					filterChain
